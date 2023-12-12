089eeee59c124f24f886d3c7e1fc6d55a219227c
Nareman <naremansharkan5@gmail.com>
2023-12-13 00:25:38 +0200

    add showWinAlert methode in AlertMessage to show Congratulation video

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -9,13 +9,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tictactoe.login.LoginScreenBase;
 
 
 public class TicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = new XOgameUI();
+        Parent root = new LoginScreenBase();
         String image = TicTacToe.class.getResource("app.jpg").toExternalForm();
         root.setStyle("-fx-background-image: url('" + image + "'); "+
                  "-fx-background-size: 100% 100%;"+
