ce2972c1d6f4efd4fba0e9518fe60fd9dc5d38cc
Shimaa <shimaasamy460@gmail.com>
2023-12-13 16:02:52 +0200

    Adding Logic For playing with AI

--- src/homePage/XoGame.java
+++ src/homePage/XoGame.java
@@ -10,25 +10,45 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import gameBoard.GameBoardUI;
+import gameBoard.ChooseGameUI;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 
 /**
  *
  * @author DELL
  */
 public class XoGame extends Application {
+    private boolean playWithComputer;
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = new XOgameUI();
+        Parent root = new GameBoardUI();
         String image = XOgameUI.class.getResource("bkdnd.jpg").toExternalForm();
         root.setStyle("-fx-background-image: url('" + image + "'); "+
                  "-fx-background-size: 100% 100%;"+
                  "-fx-background-position: center center;");
         
-        Scene scene = new Scene(root);
         
-        stage.setScene(scene);
+         Scene gameScene;
+        if (playWithComputer) {
+            GameBoardUI gameBoard = new GameBoardUI(true); // true indicates playing with the computer
+            gameScene = new Scene(gameBoard, 600, 400);
+        } 
+        else {
+            GameBoardUI gameBoard = new GameBoardUI(); // Example, adjust based on your actual class
+            gameScene = new Scene(gameBoard, 600, 400);
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(gameScene);
+        stage.setTitle("Game Board");
         stage.show();
+
+        // Close the player choice window
+       // Stage primaryStage = (Stage) playWithComputer.getScene().getWindow();
+       // primaryStage.close();
     }
 
     /**
