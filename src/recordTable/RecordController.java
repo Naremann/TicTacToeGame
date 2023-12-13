d4a435d2d233c20b7377ae98f5e52d5c48a33536
Shimaasami400 <114086723+Shimaasami400@users.noreply.github.com>
2023-12-13 20:34:52 +0200

    Revert "History UI"

--- src/recordTable/RecordController.java
+++ src/recordTable/RecordController.java
@@ -3,39 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package recordmenu;
+package recordTable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 
 /**
+ * FXML Controller class
  *
  * @author DELL
  */
 public class RecordController implements Initializable {
-    
-   
-    private ListView<String> myListView;
-    
-    private Label label;
-    
-    String[] myRecord={"SAMI","",""};
-    
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-       
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        myListView.getItems().addAll(myRecord);
+        // TODO
     }    
     
 }
