1ff83a6a83ae67f3cff2ba971df198e845860c56
IbrahimMarey <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>
2023-12-14 00:27:36 +0200

    merge

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -9,17 +9,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import recordTable.RecordList;
-import tictactoe.login.LoginScreenBase;
+import tictactoe.gamelevels.GameLevelsBase;
 
 
 public class TicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-
         Parent root = new XOgameUI();
-
         String image = TicTacToe.class.getResource("app.jpg").toExternalForm();
         root.setStyle("-fx-background-image: url('" + image + "'); "+
                  "-fx-background-size: 100% 100%;"+
